b9ebd5f2786f110b4cf49885b7cbe62365f1fe41
fitri00png <fitri00.png@gmail.com>
2026-05-21 19:33:02 +0800

    Add files via upload

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,15 @@
 
-import './globals.css'
+import "./globals.css";
 
 export const metadata = {
-  title: 'Apostrophy Creative',
-}
+  title: "Apostrophy Creative",
+  description: "Premium aesthetic sentimental gifts",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
