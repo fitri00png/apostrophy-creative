b0b7834b5dab7c7cfb16f501ecb82194f7691e12
fitri00png <fitri00.png@gmail.com>
2026-05-21 19:46:30 +0800

    Add files via upload

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+
+export const metadata = {
+  title: 'Apostrophy Creative',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  )
+}
